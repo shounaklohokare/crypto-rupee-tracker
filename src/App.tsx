a15246a83222603ff09be1a27d109e172a28e5ff
Shounak Lohokare <shounaklohokare@gmail.com>
2024-05-17 23:55:37 +0530

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <div className="min-h-screen bg-slate-200">
+            <div className="absolute top-0 h-24 w-full flex justify-center text-3xl bg-slate-950">
+                <h1 className="uppercase text-white my-4 font-mono tracking-wider pt-4">Crypto Rupee Tracker</h1>
+            </div>
+        </div>
+
 }
 
 export default App
